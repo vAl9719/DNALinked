ee6c587616f9ecde69480229fd860a65aa6349f2
Owen Astrachan <ola@cs.duke.edu>
2020-09-25 01:01:49 -0400

    initial code commit

---
+++ src/LinkStrand.java
@@ -0,0 +1,36 @@
+public class LinkStrand implements IDnaStrand {
+    @Override
+    public long size() {
+        return 0;
+    }
+
+    @Override
+    public void initialize(String source) {
+
+    }
+
+    @Override
+    public IDnaStrand getInstance(String source) {
+        return null;
+    }
+
+    @Override
+    public IDnaStrand append(String dna) {
+        return null;
+    }
+
+    @Override
+    public IDnaStrand reverse() {
+        return null;
+    }
+
+    @Override
+    public int getAppendCount() {
+        return 0;
+    }
+
+    @Override
+    public char charAt(int index) {
+        return 0;
+    }
+}
